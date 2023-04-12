62abb17ce1df6908ed5cd00f3ed18bfda53912c5
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-12 22:53:35 +0530

    First Code(NodeJs)

---
+++ nodeTut1.js
@@ -0,0 +1,5 @@
+// We can run this in command prompt as well 
+// Usme bas hame jo yaha likha hai vaha par likhna hai 
+// To start it write node 
+// Uske baad sab run ho jayega
+console.log("Namaste");
