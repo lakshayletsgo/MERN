5c1968858e832e84079094f8b572c073e44564af
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-13 13:44:04 +0530

    ECMA Script

---
+++ nodeTut4.js
@@ -0,0 +1,4 @@
+// ECMA Script 
+// let, const, templating string, class
+
+// var and let are almost same bas var me ham jitni baar bhi redefine kar skte hai aur let me ek baar define kar diya toh poore script me vahi rahega
