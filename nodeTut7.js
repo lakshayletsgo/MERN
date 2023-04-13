a803910ee780c73537ed5fd934092f2601c5ed8f
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-13 18:24:18 +0530

    http

--- nodeTut7.js
+++ nodeTut7.js
@@ -19,3 +19,7 @@ const server = http.createServer(function(req,res){
 }).listen(3000,()=>{
     console.log(`Server running on port 3000`);
 });
+
+
+// Iske baad nodemon istallation tha and nothing new to learn 
+// Bas jab bhi nodemon laga ke run kare file toh end me .js jarur lagayio varna voh index.js ko run karega
