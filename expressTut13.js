9b727267ccbc19c5433f549456e2f090c2f61b58
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-15 11:33:18 +0530

    First Pug App

---
+++ expressTut13.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.on("/",()=>{
+    console.log("This is express site");
+})
---
+++ myapp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lakshay', header:"This is header" });
+});
+
+module.exports = router;
