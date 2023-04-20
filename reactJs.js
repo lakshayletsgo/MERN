458eb1ea7cf4ee70c2a1f9c38fa294ee62fbcb7c
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-20 22:38:34 +0530

    ReactJs start

---
+++ reactJs.js
@@ -0,0 +1,18 @@
+// React is not used for any backend it is mainly use to make the site faster and smooth
+// It creates a virtual DOM where all the data is already loaded and used when asked 
+// It doesn't ask the compiler again and again for data it loads it once and use it 
+// Isme agr use karna hai toh usko ham script tag me use karte hai jo html file hai uske 
+// Uske baad usme ek function jata hai 
+// ReactDOM.render(
+// <h1>Hello,Lakshay is Here</h1>
+// document.getElementById('root')
+// );
+// Isme sabse pehle jo hame dikhana hai voh likhte hai uske baad jaha dhikhana hai voh likhte hai 
+// To start a react app we have to download it through cmd by first writing 
+
+// npx create-react-app my-app
+// After that we have to 
+// cd my-app
+// then npm start 
+// This will start the app 
+// Now after this we will do the most of the coding the app downloaded through npm 
