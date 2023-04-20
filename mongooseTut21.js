c4408d471f268633719f86abf100111046abcd86
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-20 19:40:29 +0530

    MongoDb installation

---
+++ mongooseTut21.js
@@ -0,0 +1,13 @@
+// MongoDb installation
+// This is just a readable code and not a runnable code
+// First you have to download the mongodb shell and then you have to download its server 
+// After installation of both set the environment variables of both the mongodb and mongo
+// After then you can access them through cmd 
+// First you have to write mongod and then in another cmd write mongosh 
+// After writting mongosh you can access mongodb server
+// To create a database first you have to use the database 
+// use inventory('//Name of the database') 
+// Then db.createCollection('inventory')
+// To insert a single data use db.inventory.insertOne({})
+// To insert many data use the command db.inventory.insertMany({})
+// To view the data use the command db.inventory.find({});
