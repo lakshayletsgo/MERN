421b9270728a1acf9ebabc6b5181dcf2ad631cae
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-25 22:22:43 +0530

    Props

---
+++ reactTut4.js
@@ -0,0 +1,35 @@
+// // Props 
+// // Properties are like dynamic value
+// import React from "react";
+// import ReactDOM  from 'react-dom';
+
+// // function Employee(props){
+// //   return<div>
+// //     <h1>Hello {props.name1} Side</h1>
+// //     <h1>Hello {props.name2} Side</h1>
+// //     <h1>Hello {props.name3} Side</h1>
+// //   </div>
+// // }
+// // OR
+
+// // const Employee=(props)=>{
+// //   return<div>
+// //     <h1>Hello {props.name1} Side</h1>
+// //     <h1>Hello {props.name2} Side</h1>
+// //     <h1>Hello {props.name3} Side</h1>
+// //   </div>
+// // }
+
+
+// // This is  class Component and propes are used here
+// class Employee extends React.Component{
+//   render(){
+//     return <div>
+//     <h1>Hello {this.props.name1} Side</h1>
+//     <h1>Hello {this.props.name2} Side</h1>
+//     <h1>Hello {this.props.name3} Side</h1>
+
+//     </div>
+//   }
+// }
+// ReactDOM.render(<Employee name1="lakshay" name2="Rahul" name3="manav"/>,document.getElementById('root'));
