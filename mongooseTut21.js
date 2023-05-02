6a258f8e459b6c58e6d88973f0ce6312654e65b0
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-05-03 00:35:11 +0530

    CRUD Operation(MongoDb)

--- mongooseTut21.js
+++ mongooseTut21.js
@@ -11,3 +11,39 @@
 // To insert a single data use db.inventory.insertOne({})
 // To insert many data use the command db.inventory.insertMany({})
 // To view the data use the command db.inventory.find({});
+
+// This will give all the object having status D
+// To find a particular data use db.(name of the folder).find({status: "D"})
+
+
+// This will give data having status A and D
+// db.inventory.find( { status: { $in: [ "A", "D" ] } } )
+
+
+//This will give data having status A and quantity less than 30 
+// db.inventory.find( { status: "A", qty: { $lt: 30 } } )
+
+// This will give data having status A or quantity less than 30
+// db.inventory.find( { $or: [ { status: "A" }, { qty: { $lt: 30 } } ] } )
+
+
+// $lt => is less than
+// $lte => is less than or equal 
+// $gt => is greater than
+// $gte =>  is greater than or equal
+// $in => in the array 
+// $eq => is equal 
+// $ne => not equal
+// $nin => not in the array 
+
+
+// To update any data use the function
+// db.inventory.updateOne({condition},{$set:{what to update}})
+// db.inventory.updateOne({item:"postcard"},{$set:{qty:"34"}})
+// db.inventory.updateMany({item:"postcard"},{$set:{qty:"34"}})
+
+
+// To delete any data use the function
+// db.inventory.deleteOne({what to delete})
+// db.inventory.deleteOne( { status: "D" } )
+// db.inventory.deleteMany({ status : "A" })
