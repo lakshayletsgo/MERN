236043da48ffef91fec85ab6ca208083d6992e27
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-05-04 20:16:55 +0530

    Indexes in Mongodb

---
+++ mongooseTut25.js
@@ -0,0 +1,10 @@
+// Indexes in Mongodb
+// It is used so that the finding speed increases
+// It is default created for the object id 
+// We can change it 
+
+// db.orders.getIndexes()
+// It will give the current indexes of the database
+
+//   db.orders.createIndex({"email":1})
+//It will create an index for the email 
