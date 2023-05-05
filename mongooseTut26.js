6d3d1f9ce3badfd84dcba5dfe294a39d72cbe1bf
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-05-05 20:15:58 +0530

    BackUp and Restore

---
+++ mongooseTut26.js
@@ -0,0 +1,7 @@
+// BackUp and Restore
+// To create a backup you first need to move that directory( where u want to store the data) using cd function
+// Then just use the key word mongodump 
+
+// Restore
+// To restore you need to be in that directory where it is backup then
+//  use the cmd mongorestore
